b2115a0c8d682018a27631598939642650e24e93
elyk <kconnors51@gmail.com>
2014-02-22 00:17:29 -0800

    adding stripe library

--- evenup/evenup/EventAdminViewController.m
+++ evenup/evenup/EventAdminViewController.m
@@ -19,6 +19,7 @@
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view.
+    self.title = @"Event Admin";
 }
 
 - (void)didReceiveMemoryWarning
--- evenup/evenup/PaymentViewController.m
+++ evenup/evenup/PaymentViewController.m
@@ -7,7 +7,7 @@
 //
 
 #import "PaymentViewController.h"
-
+#import "STPView.h"
 @interface PaymentViewController ()
 
 @end
@@ -28,7 +28,13 @@
     [super viewDidLoad];
 	// Do any additional setup after loading the view.
     self.title = @"Payment Details";
+    
     [self setLeftMenuButton];
+    
+    STPView *stripeView = [[STPView alloc] initWithFrame:CGRectMake(20, 50, self.view.frame.size.width-40, 300) andKey:@"pk_test_rYhRKdREwHaMp6KoaveWVNsE"];
+    
+    [self.view addSubview:stripeView];
+
 }
 
 - (void)didReceiveMemoryWarning
